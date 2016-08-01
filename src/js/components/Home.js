988ed8176ec741b5dfe409d389c2b50e753c10a7
karatechops <contact@lazers.tv>
2016-08-01 13:40:32 -0400

    Removed scss import.

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -16,7 +16,7 @@ import Section1 from '../sections/Section1';
 import Section2 from '../sections/Section2';
 import Nav from './Nav';
 
-import '../../scss/index.scss';
+//import '../../scss/index.scss';
 
 // Global timer to monitor scrolling.
 let SCROLL_TIMER;
